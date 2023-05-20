4adf0b848d0f864cff2d9e5340991e2993be7c44
nidoranz2 <58795303+nidoranz2@users.noreply.github.com>
2023-05-20 15:56:55 +0900

    Added comments

--- romanToInt.c
+++ romanToInt.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+//Creating a function named value that accepts a Roman Numeral Character and returns the equivalent value of the Roman Numeral
 int value(char c)
 {
     switch(c){
@@ -23,6 +24,7 @@ int value(char c)
         return 0;
     }
 }
+//function that calculates the value of the Roman Numeral according to its arrangement
 int romanToInt(char * s){
     int total = 0;
     for(int i = 0; i < strlen(s) ; i++){
@@ -39,6 +41,7 @@ int romanToInt(char * s){
 }
 int main(int argc, char *argv[])
 {
+    //ends the program if the program is initiated incorrectly
     if(argc > 2 && !isdigit(*argv[1]))
     {
         printf("Too much arguments. Correct usage: ./romantoint [Roman Numeral]");
